83da229ba42f3913615b26610ebcc8ff6a87104b
abdy1988 <gapyr.mb@gmail.com>
2025-05-31 01:58:52 +0000

    some scripts

---
+++ function.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+function persmission() {
+	sudo chown tim folder1
+	sudo chown ann file1
+}
+function user() {
+	sudo useradd tim
+	sudo useradd ann
+	sudo useradd brad
+}
+
+function file() { 
+	mkdir folder1
+	touch file1
+	touch folder/new
+}
+
+file
+permission
+user
+
+	
